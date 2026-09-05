aa796a750b53fbb5371bd9815611b15e4c4efc7e
abhaykushwahtech27012008 <abhaysk27012008@gmail.com>
2026-09-05 20:08:44 +0530

    feat: add first c lab program (hello world)

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Displaying a professional welcome message for B.Tech First Year
+    printf("Hello, World! Welcome to Abhay's C Programming Lab.\n");
+    printf("B.Tech CSE - First Semester (2026 Batch)\n");
+    
+    return 0;
+}
